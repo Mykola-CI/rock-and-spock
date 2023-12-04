ab125dd7f70de80b91d5d60b3c1922bb69b36fee
Mykola Kuievda <dankraig23@gmail.com>
2023-12-04 00:35:10 +0000

    Add code to change screen from playground to display hands and countdown

--- index.js
+++ index.js
@@ -1,33 +1,42 @@
+document.addEventListener("DOMContentLoaded", function () {
+  displaySection("intro");
+  hideSection("playground");
+  hideSection("clock-countdown");
+  hideSection("hand-selection");
+  hideSection("footer--score");
+  hideSection("footer--round-count");
 
+  const goPlaygroundButton = document.getElementById("start-playground--button");
 
-document.addEventListener("DOMContentLoaded", function() {
-    
-    displaySection("intro");
-    hideSection("playground");
+  goPlaygroundButton.addEventListener("click", function () {
+    hideSection("intro");
+    displaySection("playground");
     hideSection("clock-countdown");
     hideSection("hand-selection");
     hideSection("footer--score");
     hideSection("footer--round-count");
-
-    const goPlaygroundButton = document.getElementById("start-playground--button");
-
-    goPlaygroundButton.addEventListener("click", function() {
-        hideSection("intro");
-        displaySection("playground");
-        hideSection("clock-countdown");
-        hideSection("hand-selection");
-        hideSection("footer--score");
-        hideSection("footer--round-count");
-    })
-})
+  });
+});
 
 function hideSection(id) {
-    const hideElement = document.getElementById(id);
-    hideElement.classList.add("hidden");
+  const hideElement = document.getElementById(id);
+  hideElement.classList.add("hidden");
 }
 
 function displaySection(id) {
-    const displayElement = document.getElementById(id);
-    displayElement.classList.remove ("hidden");
+  const displayElement = document.getElementById(id);
+  displayElement.classList.remove("hidden");
 }
 
+const hands = ["rock", "paper", "scissors", "lizard", "spock"];
+const startRoundBtn = document.getElementById("start-control--button");
+
+startRoundBtn.addEventListener("click", function () {
+  hideSection("intro");
+  hideSection("playground");
+  displaySection("clock-countdown");
+  displaySection("hand-selection");
+  hideSection("footer--score");
+  hideSection("footer--round-count");
+
+});
