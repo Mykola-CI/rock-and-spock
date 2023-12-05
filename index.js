9acb6569d5e9b24291855fc44db0ba9a0adadb95
Mykola Kuievda <dankraig23@gmail.com>
2023-12-05 22:56:16 +0000

    Add two functions: hand pick handler and listeners remover after round finish

--- index.js
+++ index.js
@@ -42,11 +42,13 @@ function displaySection(id) {
 
 // Array that consists of strings - id selectors of 5 available gaming hands
 const hands = ["rock", "paper", "scissors", "lizard", "spock"];
-
+const handButtons = document.querySelectorAll(".hand-type--button");
 const startRoundBtn = document.getElementById("start-control--button");
+let timeoutID;
 
 // Setting event listener for the only button on the Playground screen
 startRoundBtn.addEventListener("click", function () {
+
   // Change the Playground screen to display 5 hands and clock-ticking
   hideSection("intro");
   hideSection("playground");
@@ -56,57 +58,61 @@ startRoundBtn.addEventListener("click", function () {
   hideSection("footer--score");
   hideSection("footer--round-count");
 
-  let indexComp = Math.floor(Math.random() * 5);
-  console.log("This is Computers choice: " + indexComp); // Console
-
-  let timeoutID = setTimeout(function () {
-    indexUser = 5;
-    console.log(indexComp + ": nobody seems to have picked the hand"); // Console
+  timeoutID = setTimeout(function () {
     hideSection("clock-countdown");
     hideSection("hand-selection");
     displaySection("display-result");
-    defineWinner(indexComp, indexUser);
+    defineWinner(5);
   }, 3000);
 
-  hands.forEach((hand, indexUser) => {
-    document.getElementById(hand).addEventListener("click", function () {
-      clearTimeout(timeoutID);
-      console.log("This is User's choice: " + hand); // Console
-      console.log("This is User's index: " + indexUser); // Console
-      hideSection("clock-countdown");
-      displaySection("display-result");
-      defineWinner(indexComp, indexUser);
-    });
+  handButtons.forEach((handButton) => {
+    handButton.addEventListener("click", handClickHandler);
   });
 });
 
-function defineWinner(compHand, userHand) {
-  console.log("This is what happened:"); // Console
-  console.log(compHand + "_versus_" + userHand); // Console
+function handClickHandler(event) {
+  const clickedHandId = event.currentTarget.id;
+  clearTimeout(timeoutID);
+  hideSection("clock-countdown");
+  displaySection("display-result");
+  console.log("this is the test for retrieving id: " + clickedHandId); // console
+  const indexUser = hands.indexOf(clickedHandId);
+  defineWinner(indexUser);
+}
+
+function removeHandEventListeners() {
+  handButtons.forEach(handButton => {
+    handButton.removeEventListener('click', handClickHandler);
+  });
+}
+
+function defineWinner(userHand) {
+  
+  let indexComp = Math.floor(Math.random() * 5);
+
 
   for (let index = 0; index < hands.length; index++) {
-    if (index !== userHand && index !== compHand) {
+    if (index !== userHand && index !== indexComp) {
       hideSection(hands[index]);
     }
   }
 
-  if (compHand === userHand) {
-    document.getElementById("display-result--title").innerText = "Damn! It's a tie!"
+  if (indexComp === userHand) {
+    document.getElementById("display-result--title").innerText = "Damn! It's a tie!";
     console.log("Damn it! It's a tie!"); // Console
   } else if (
-    (userHand === 0 && (compHand === 2 || compHand === 3)) ||
-    (userHand === 1 && (compHand === 0 || compHand === 4)) ||
-    (userHand === 2 && (compHand === 1 || compHand === 3)) ||
-    (userHand === 3 && (compHand === 1 || compHand === 4)) ||
-    (userHand === 4 && (compHand === 0 || compHand === 2))
+    (userHand === 0 && (indexComp === 2 || indexComp === 3)) ||
+    (userHand === 1 && (indexComp === 0 || indexComp === 4)) ||
+    (userHand === 2 && (indexComp === 1 || indexComp === 3)) ||
+    (userHand === 3 && (indexComp === 1 || indexComp === 4)) ||
+    (userHand === 4 && (indexComp === 0 || indexComp === 2))
   ) {
-    document.getElementById("display-result--title").innerText = `You win! ${hands[userHand]} beats ${hands[compHand]}`;
-    console.log(`You win! ${hands[userHand]} beats ${hands[compHand]}`); // Console
+    document.getElementById("display-result--title").innerText = `You win! ${hands[userHand]} beats ${hands[indexComp]}`;
   } else if (userHand === 5) {
     document.getElementById("display-result--title").innerText = `Oi! You haven't picked a hand! Pity, but you loose anyway!`;
-    console.log(`Oi! You haven't picked a hand! Pity, but you loose anyway!`); // Console
   } else {
-    document.getElementById("display-result--title").innerText = `Ups! Bad Luck! ${hands[compHand]} beats ${hands[userHand]}`;
-    console.log(`Ups! Bad Luck! ${hands[compHand]} beats ${hands[userHand]}`); // Console
+    document.getElementById("display-result--title").innerText = `Ups! Bad Luck! ${hands[indexComp]} beats ${hands[userHand]}`;
   }
+
+  removeHandEventListeners();
 }
