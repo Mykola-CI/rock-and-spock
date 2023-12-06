93e93f46c41c694590e3a7fc1ee62461d7693cdc
Mykola Kuievda <dankraig23@gmail.com>
2023-12-06 00:58:14 +0000

    Add two functions to display Intro and Playground screens, add Window load event

--- index.js
+++ index.js
@@ -1,44 +1,14 @@
-// Setting event listener on the DOM load
-document.addEventListener("DOMContentLoaded", function () {
-  // Arranging visibility of DOM elements on the first (landing) screen
-  displaySection("intro");
-  hideSection("playground");
-  hideSection("clock-countdown");
-  hideSection("display-result");
-  hideSection("hand-selection");
-  hideSection("footer--score");
-  hideSection("footer--round-count");
+// Setting event listener on the Window load
+window.onload = (event) => {
+  displayIntroScreen();
 
   const goPlaygroundButton = document.getElementById("start-playground--button");
 
   // Setting event listener for the only button on the intro screen
   goPlaygroundButton.addEventListener("click", function () {
-    // Change the the Intro screen to the Playground one
-    hideSection("intro");
-    displaySection("playground");
-    hideSection("clock-countdown");
-    hideSection("display-result");
-    hideSection("hand-selection");
-    hideSection("footer--score");
-    hideSection("footer--round-count");
+    displayPlayground();
   });
-});
-
-/**
- * Function hiding the section with the specified id
- */
-function hideSection(id) {
-  const hideElement = document.getElementById(id);
-  hideElement.classList.add("hidden");
-}
-
-/**
- * Function displaying the section with the specified id
- */
-function displaySection(id) {
-  const displayElement = document.getElementById(id);
-  displayElement.classList.remove("hidden");
-}
+};
 
 // Array that consists of strings - id selectors of 5 available gaming hands
 const hands = ["rock", "paper", "scissors", "lizard", "spock"];
@@ -48,7 +18,6 @@ let timeoutID;
 
 // Setting event listener for the only button on the Playground screen
 startRoundBtn.addEventListener("click", function () {
-
   // Change the Playground screen to display 5 hands and clock-ticking
   hideSection("intro");
   hideSection("playground");
@@ -70,6 +39,57 @@ startRoundBtn.addEventListener("click", function () {
   });
 });
 
+/**
+ * A combination of hide and display function calls is grouped
+ * to display Introductory Screen
+ */
+function displayIntroScreen() {
+  displaySection("intro");
+  hideSection("playground");
+  hideSection("clock-countdown");
+  hideSection("display-result");
+  hideSection("hand-selection");
+  hideSection("footer--score");
+  hideSection("footer--round-count");
+}
+
+/**
+ * A combination of hide and display function calls is grouped
+ * to display Playground Screen with Start Round button
+ */
+function displayPlayground() {
+  hideSection("intro");
+  displaySection("playground");
+  hideSection("clock-countdown");
+  hideSection("display-result");
+  hideSection("hand-selection");
+  hideSection("footer--score");
+  hideSection("footer--round-count");
+}
+
+/**
+ * Function hiding the section with the specified id
+ * @param {*} id
+ */
+function hideSection(id) {
+  const hideElement = document.getElementById(id);
+  hideElement.classList.add("hidden");
+}
+
+/**
+ * Function displaying the section with the specified id
+ * @param {*} id
+ */
+function displaySection(id) {
+  const displayElement = document.getElementById(id);
+  displayElement.classList.remove("hidden");
+}
+
+/**
+ * Rearranges the screen to reveal result and clears timeout
+ * to prevent defineWinner function run the second time
+ * @param {*} event
+ */
 function handClickHandler(event) {
   const clickedHandId = event.currentTarget.id;
   clearTimeout(timeoutID);
@@ -80,16 +100,24 @@ function handClickHandler(event) {
   defineWinner(indexUser);
 }
 
+/**
+ * Remove event listeners for 5 hand buttons after
+ * the round has been finished and Winner defined. The goal is
+ * to prevent from triggering of multiple code running on accidental clicks
+ */
 function removeHandEventListeners() {
-  handButtons.forEach(handButton => {
-    handButton.removeEventListener('click', handClickHandler);
+  handButtons.forEach((handButton) => {
+    handButton.removeEventListener("click", handClickHandler);
   });
 }
 
+/**
+ * Major function that randomly generates computer's hand
+ * Executes the logic of defining the winner and displaying the result messages 
+ * @param {*} userHand 
+ */
 function defineWinner(userHand) {
-  
   let indexComp = Math.floor(Math.random() * 5);
-
 
   for (let index = 0; index < hands.length; index++) {
     if (index !== userHand && index !== indexComp) {
